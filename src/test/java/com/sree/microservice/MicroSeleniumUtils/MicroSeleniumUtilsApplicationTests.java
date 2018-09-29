8edef5010463c1f64f96dc06d56a1d1c62bb4155
sree <sreenkc83@gmail.com>
2018-09-29 03:11:19 -0500

    updating them now

---
+++ src/test/java/com/sree/microservice/MicroSeleniumUtils/MicroSeleniumUtilsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.sree.microservice.MicroSeleniumUtils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroSeleniumUtilsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+	@Test
+	public void contextLoads1() {
+	}
+// added mine too
+}
